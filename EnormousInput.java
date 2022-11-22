8aff466d5f956fb46bf10655bacec033fc4c9d89
Nikhil <nikhilbisht0026@gmail.com>
2022-11-22 21:52:51 +0530

    Enormous Input Test

---
+++ EnormousInput.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class EnormousInput
+{
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int ctr=0;
+		
+		while(n>0){
+		    int a=sc.nextInt();
+		    if(a%k==0){
+		        ctr++;
+		    }
+		    n--;
+		}
+		System.out.println(ctr);
+        sc.close();
+	}
+}
