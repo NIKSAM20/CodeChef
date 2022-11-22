c002cf9840c697bb369ed13cda06b539d79ff5c4
Nikhil <nikhilbisht0026@gmail.com>
2022-11-22 21:59:25 +0530

    Find the Remainder

---
+++ FindRemainder.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class FindRemainder
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int t=sc.nextInt();
+		while(t>0){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    
+		    int remainder= a%b;
+            System.out.println(remainder);
+            t--;
+		}
+        sc.close();
+	}
+}
