5db306a7f1d669de43a873d54f265a5e20308bb3
Nikhil <nikhilbisht0026@gmail.com>
2022-11-22 20:53:35 +0530

    Brain speed threshold limit

---
+++ BrainSpeed.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class BrainSpeed
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		    
+		if(y>x){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}		
+        sc.close();
+	}
+}
