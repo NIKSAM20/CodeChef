d28493953fc1c2ca4b3d5af9d571f465e7c766c4
Nikhil <nikhilbisht0026@gmail.com>
2022-11-22 20:39:18 +0530

    Eligible to ride on the Roller Coaster

---
+++ RollerCoaster.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class RollerCoaster
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		while(n>0){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    
+		    if(x>=y){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		    n--;
+		}
+        sc.close();
+	}
+}
