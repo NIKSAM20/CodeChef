6371bcf6523f216f40737c05d345f6d2158dfe67
Nikhil <nikhilbisht0026@gmail.com>
2022-11-22 22:08:17 +0530

    Sum of Digits of numbers

---
+++ SumofDigits.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class SumofDigits
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int t=sc.nextInt();
+		while(t>0){
+		    int n=sc.nextInt();
+		    
+		    int sum=0;
+		    while(n>0){
+		        int rem=n%10;
+		        sum=sum+rem;
+		        n=n/10;
+		    }
+		    System.out.println(sum);
+            t--;
+		}
+        sc.close();
+	}
+}
