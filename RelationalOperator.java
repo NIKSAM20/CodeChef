75e95623fc1271ab14af3bd9dfbdf24b5270ea85
Nikhil <nikhilbisht0026@gmail.com>
2022-11-23 10:43:10 +0530

    Relational Operators

---
+++ RelationalOperator.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class RelationalOperator
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		while(n!=0){
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		
+		    if(a<b){
+		        System.out.println("<");
+		    }
+		    else if(a==b){
+		        System.out.println("=");
+		    }
+		    else{
+		        System.out.println(">");
+		    }
+		    n--;
+		}    
+        sc.close();
+	}
+}
