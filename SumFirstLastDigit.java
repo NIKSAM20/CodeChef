d94be4985856edfb6e0e3ca7821971a0c19c87cf
Nikhil <nikhilbisht0026@gmail.com>
2022-11-24 10:19:17 +0530

    Sum of the first and last digit of a number

---
+++ SumFirstLastDigit.java
@@ -0,0 +1,21 @@
+public class SumFirstLastDigit {
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n,t,sum=0,rem=0;
+		
+		t=sc.nextInt();
+		while(t!=0){
+		    
+		    n=sc.nextInt();
+		    sum=n%10;
+		    while(n!=0){
+		        rem=n%10;
+		        n=n/10;    
+		    }
+		    sum=sum+rem;
+		    System.out.println(sum);
+		    t--;    
+		}
+		sc.lose();
+	}
+}
