521353460939f040c9f216ee73e5b10ba61fa0ac
Nikhil <nikhilbisht0026@gmail.com>
2022-11-24 09:55:00 +0530

    Transaction in the ATM

---
+++ ATM.java
@@ -0,0 +1,16 @@
+class ATM{
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		float x=sc.nextInt();
+		float y=sc.nextFloat();
+		
+		if((x%5==0) && (y>=x+0.5f)){
+		    float bal=y-x-0.5f;
+		    System.out.println(bal);
+		}
+		else{
+		    System.out.println(y);
+		}
+	}
+}
