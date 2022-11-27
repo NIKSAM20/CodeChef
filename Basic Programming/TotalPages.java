fc371d7147576d37661f9a4898803989591a00cf
Nikhil <nikhilbisht0026@gmail.com>
2022-11-27 19:12:57 +0530

    Total pages in the book

---
+++ Basic Programming/TotalPages.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class TotalPages {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc= new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t>0){
+		    int m=sc.nextInt();
+		    int n=sc.nextInt();
+		   
+		    System.out.println(x*y);
+		    t--;
+		}
+	}
+}
