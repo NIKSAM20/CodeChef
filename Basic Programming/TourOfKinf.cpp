501efccd0bef7a66dabb233c83eea7e0b03114f4
Nikhil <nikhilbisht0026@gmail.com>
2022-12-01 19:57:26 +0530

    Total number of people in the cars

---
+++ Basic Programming/TourOfKinf.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes herea
+	int t, m, n;
+	cin >>t;
+    while (t>0){
+        cin >>n;
+        cin >>m;
+        cout <<(n*5)+(m*7) <<"\n";
+        t--;
+    }	
+	return 0;
+}
