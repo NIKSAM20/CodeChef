dd57372cffb959ee9e67078a1bd10f6fe192a4f0
Nikhil <nikhilbisht0026@gmail.com>
2022-12-03 22:19:43 +0530

    Total number of credits in the semester

---
+++ Basic Programming/Credits.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t, x, y, z;
+	cin >>t;
+	while(t>0){
+	    cin >>x >>y >>z;
+	    
+	    cout <<(4*x)+(2*y) <<"\n";
+	    t--;
+	}
+	return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t, x, y, z;
+	cin >>t;
+	while(t>0){
+	    cin >>x >>y >>z;
+	    
+	    cout <<(4*x)+(2*y) <<"\n";
+	    t--;
+	}
+	return 0;
+}
