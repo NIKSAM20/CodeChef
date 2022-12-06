e2a697b2ba7451466265045ffedc03a3c69c0daa
Nikhil <nikhilbisht0026@gmail.com>
2022-12-06 19:49:33 +0530

    Reccommended rated questions to improve your rating

---
+++ Basic Programming/RatingImprovement.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t, x, y;
+	cin >>t;
+	while(t>0){
+	    cin >>x >>y;
+	    if (y>x && y<=x+200){
+	        cout <<"YES" <<"\n";
+	    }
+	    else{
+	        cout <<"NO" <<"\n";
+	    }
+	    t--;
+	}
+	return 0;
+}
