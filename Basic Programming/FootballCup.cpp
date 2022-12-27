1011a6881af1065e62a39e25d5c9ddfa4609773f
Nikhil <nikhilbisht0026@gmail.com>
2022-12-27 22:43:40 +0530

    Goals scored in the match

---
+++ Basic Programming/FootballCup.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t, c, y;
+	cin >>t;
+	while(t>0){
+	    cin >>x >>y;
+	    if((x==y) && (x+y>0)){
+	        cout <<"YES" <<"\n";
+	    }
+	    else{
+	        cout <<"NO" <<"\n";
+	    }
+	    
+	    t--;
+	}
+	return 0;
+}
