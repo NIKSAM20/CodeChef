2afce21d341d161eae469fa1bcd7a821bc8d990e
Nikhil <nikhilbisht0026@gmail.com>
2022-12-29 22:42:45 +0530

    Money spent by the Chef to gift the chocolates:

---
+++ Basic Programming/Chocolates.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t, c, x, y;
+	cin >>t;
+	while(t>0){
+	    cin >>c >>x >>y;
+	    cout <<(c-x)*y <<"\n";
+	    t--;
+	}
+	return 0;
+}
