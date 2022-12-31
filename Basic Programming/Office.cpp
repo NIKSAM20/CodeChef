3a17de1034c58bfe675020dfb5ccd5b011d650fb
Nikhil <nikhilbisht0026@gmail.com>
2022-12-31 22:40:51 +0530

    Total number of working hours

---
+++ Basic Programming/Office.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t, x, y;
+	cin >>t;
+	while(t>0){
+	    cin >> x >>y;
+	    cout <<4*x+y <<"\n";
+	    
+	    t--;
+	} 
+	return 0;
+}
